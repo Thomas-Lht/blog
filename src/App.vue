cdc1eb896f65121a1a6b69437e4b67d54354ade7
今夕何夕 <luhaotian3936848@gmail.com>
2024-10-11 13:42:00 +0800

    Changes to be committed: 	modified:   src/App.vue 	deleted:    src/components/Footer.vue 	deleted:    src/components/Header.vue 	deleted:    src/components/Navbar.vue 	deleted:    src/components/ServerStatus.vue 	deleted:    src/components/Weather.vue  Untracked files: 	public/images/ 	src/components/AppFooter.vue 	src/components/AppHeader.vue 	src/components/AppNavbar.vue 	src/components/AppServerStatus.vue 	src/components/AppWeather.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="app"> <!-- 注意这里的 class 名称为小写 -->
-    <Header />
-    <Navbar />
-    <Footer />
+    <AppHeader />
+    <AppNavbar />
+    <AppFooter />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppNavbar from './components/AppNavbar.vue';
+import AppFooter from './components/AppFooter.vue';
 // 如果需要使用 ServerStatus 和 Weather 组件，可以将它们也引入
 // import ServerStatus from './components/ServerStatus.vue';
 // import Weather from './components/Weather.vue';
@@ -17,11 +17,9 @@ import Footer from './components/Footer.vue';
 export default {
   name: 'App',
   components: {
-    Header,
-    Navbar,
-    Footer,
-    // ServerStatus,
-    // Weather
+    AppHeader,
+    AppNavbar,
+    AppFooter,
   }
 };
 </script>
